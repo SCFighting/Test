2e88edd14c51330e6b64a21ac7b58c06d1aedb4c
SC <sunweichao@rayclear.com>
2021-07-06 14:09:01 +0800

    重置

--- Test/Home/Controller/HomeController.m
+++ Test/Home/Controller/HomeController.m
@@ -9,7 +9,15 @@
 #import <GPUImage/GPUImage.h>
 #import <Masonry/Masonry.h>
 @interface HomeController ()<GPUImageVideoCameraDelegate>
+//摄像头输出
 @property (strong, nonatomic) GPUImageVideoCamera *videoCamera;
+//图片输出
+@property (strong, nonatomic) GPUImagePicture *pictureOutPut;
+//图片处理
+@property (strong, nonatomic) GPUImageSepiaFilter *stillImageFilter;
+//双输入
+@property (strong, nonatomic) GPUImageTwoInputFilter *twoInputFilter;
+//渲染
 @property (strong, nonatomic) GPUImageView *randerView;
 @end
 
@@ -26,16 +34,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.videoCamera addTarget:self.randerView];
-    [self.videoCamera startCameraCapture];
+    [self.pictureOutPut addTarget:self.stillImageFilter];
+    [self.stillImageFilter addTarget:self.randerView];
+    
+    [self.pictureOutPut processImage];
+    [self.stillImageFilter useNextFrameForImageCapture];
+    [self.stillImageFilter setFrameProcessingCompletionBlock:^(GPUImageOutput *,CMTime) {
+        NSLog(@"------------");
+    }];
+    
     // Do any additional setup after loading the view.
 }
 
 #pragma mark -- GPUImageVideoCameraDelegate
-
--(void)willOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer output:(AVCaptureOutput *)output
-{
-}
+//
+//-(void)willOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer output:(AVCaptureOutput *)output
+//{
+//}
 
 #pragma mark -- Getter
 
@@ -44,9 +59,33 @@
     if (_videoCamera == nil) {
         _videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1280x720 cameraPosition:AVCaptureDevicePositionFront];
         _videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
-        _videoCamera.delegate = self;
     }
     return _videoCamera;
+}
+
+- (GPUImagePicture *)pictureOutPut
+{
+    if (!_pictureOutPut) {
+        UIImage *image = [UIImage imageNamed:@"1242-2688"];
+        _pictureOutPut = [[GPUImagePicture alloc] initWithImage:image];
+    }
+    return _pictureOutPut;
+}
+
+-(GPUImageSepiaFilter *)stillImageFilter
+{
+    if (!_stillImageFilter) {
+        _stillImageFilter = [[GPUImageSepiaFilter alloc] init];
+    }
+    return _stillImageFilter;
+}
+
+-(GPUImageTwoInputFilter *)twoInputFilter
+{
+    if (!_twoInputFilter) {
+        _twoInputFilter = [[GPUImageTwoInputFilter alloc] init];
+    }
+    return _twoInputFilter;
 }
 
 -(GPUImageView *)randerView
